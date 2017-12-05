d0a942b4da72a99d81f7663e37c0bf836f5e1096
grushaprasad <grushaprasad@Grushas-MacBook-Air.local>
2017-12-05 17:24:34 -0500

    Increased font size of Message

--- data_includes/FineJaeger_replication_data.js
+++ data_includes/FineJaeger_replication_data.js
@@ -7,7 +7,7 @@ var defaults = [
     "Separator", {
         transfer: 1000,
         normalMessage: "Please wait for the next sentence.",
-        errorMessage: "Wrong. Please wait for the next sentence."
+        errorMessage: "Please wait for the next sentence."
     },
     "DashedSentence", {
         hideProgressBar: true,
